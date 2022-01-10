31146f32cb545035a8c845ad585c4c7a51bc47c8
Atul R <atulanand94@gmail.com>
2022-01-10 16:04:07 +0530

    bump deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-    extends: ['plugin:prettier/recommended'],
-    parserOptions: { ecmaVersion: 2018, sourceType: 'module' },
-    overrides: [
-      {
-        files: ['*.ts'],
-        parser: '@typescript-eslint/parser',
-        plugins: ['simple-import-sort'],
-        extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
-        rules: {
-          '@typescript-eslint/camelcase': 0,
-          '@typescript-eslint/no-var-requires': 0,
-          '@typescript-eslint/no-explicit-any': 0,
-          'simple-import-sort/sort': 'error',
-        },
+  extends: ['plugin:prettier/recommended'],
+  parserOptions: { ecmaVersion: 2018, sourceType: 'module' },
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['simple-import-sort'],
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+      rules: {
+        '@typescript-eslint/camelcase': 0,
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/no-explicit-any': 0,
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        '@typescript-eslint/ban-ts-comment': 0,
       },
-    ],
-  };
-  
+    },
+  ],
+};
